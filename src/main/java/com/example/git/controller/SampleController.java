cfb1b4c7f4f95a91bc356c1d366553adec492a43
kalpana <kdodla1983@gmail.com>
2020-11-15 12:01:22 -0500

    url changed hello to hello1

--- src/main/java/com/example/git/controller/SampleController.java
+++ src/main/java/com/example/git/controller/SampleController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SampleController {
 
-	 @GetMapping("/Hello")
+	 @GetMapping("/Hello1")
 	public String getData()
 	{
 		return "Chandra";
